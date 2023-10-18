01cc7ab95cfeccb18b3668513b2c98d8ee50ac06
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-18 19:09:18 +0900

    Add corsOptions to cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import mapRoutes from './routes/mapRoutes'
 import leaderboardRoutes from './routes/leaderboardRoutes'
 import cors from 'cors'
+import corsOptions from './config/corsOptions'
 
 const createServer = () => {
 
     const app = express()
     
-    app.use(cors);
+    app.use(cors(corsOptions));
     app.use(express.json())
     
     app.use('/map', mapRoutes)
