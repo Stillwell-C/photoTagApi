f8590214eb533f875f2209a1c13be29f9ec8e61b
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-19 12:30:36 +0900

    Add console log

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -6,6 +6,7 @@ const corsOptions: CorsOptions = {
         if (!origin || allowedOrigins.indexOf(origin) !== -1 ) {
             callback(null, true);
           } else {
+            console.log(origin)
             callback(Error("Not allowed by CORS"));
           }
         },
