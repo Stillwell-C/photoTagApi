f210bf303bf7a137a298999da8fd48e253b3279b
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-26 16:27:44 +0900

    Add console log

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,17 +1,18 @@
 import allowedOrigins from "./allowedOrigins";
-import {CorsOptions} from 'cors'
+import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1 ) {
-            callback(null, true);
-          } else {
-            console.log(origin)
-            callback(Error("Not allowed by CORS"));
-          }
-        },
-        optionsSuccessStatus: 200,
-        credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      console.log(origin);
+      callback(null, true);
+    } else {
+      console.log(origin);
+      callback(Error("Not allowed by CORS"));
     }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
-    export default corsOptions
+export default corsOptions;
