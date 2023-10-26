1ec496e9d7b89e4404186bc432fd34415c863671
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-26 16:40:15 +0900

    Update allowed origins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "photo-tag.vercel.app",
-  "https://photo-tag.vercel.app/",
-  "http://photo-tag.vercel.app/",
+  "https://photo-tag.vercel.app",
+  "http://photo-tag.vercel.app",
 ];
 
 export default allowedOrigins;
