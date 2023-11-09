6e3f64580b8fecda92bda010eb45ff4007b32168
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-09 18:42:54 +0900

    Remove connection with mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
 import createServer from "./app";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 3500
-const dbURI: string = process.env.DATABASE_URI!
+const PORT = process.env.PORT || 3500;
+// const dbURI: string = process.env.DATABASE_URI!
 
-const connectDB = async () => {
-    try {
-      await mongoose.connect(dbURI);
-    } catch (err) {
-      console.log(err);
-    }
-};
+// const connectDB = async () => {
+//     try {
+//       await mongoose.connect(dbURI);
+//     } catch (err) {
+//       console.log(err);
+//     }
+// };
 
-connectDB()
+// connectDB()
 
-const app = createServer()
+const app = createServer();
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to DB')
-    app.listen(PORT, () => console.log(`Server running on ${PORT}`))
-})
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+// mongoose.connection.once('open', () => {
+//     console.log('Connected to DB')
+//     app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+// })
