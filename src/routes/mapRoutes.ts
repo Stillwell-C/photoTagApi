f5c21e5267293e8b3ff92bf1095da834e69a8aa8
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-09 18:42:15 +0900

    Update to use sql db

--- src/routes/mapRoutes.ts
+++ src/routes/mapRoutes.ts
@@ -1,46 +1,58 @@
-import {Router, Request, Response} from 'express'
-import mongoose from 'mongoose'
-import Map from '../models/Map'
+import { Router, Request, Response } from "express";
+import mongoose from "mongoose";
+import Map from "../models/Map";
+import pool from "../config/dbConfig";
 
-const router = Router()
+const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
-    const mapData = await Map.find().sort("mapName").lean().exec()
+router.get("/", async (req: Request, res: Response) => {
+  // const mapData = await Map.find().sort("mapName").lean().exec()
+  const mapData = await pool.query("SELECT * FROM maps");
 
-    if (!mapData) {
-        return res.status(400).json({ message: "No map data found."})
-    }
+  if (!mapData) {
+    return res.status(400).json({ message: "No map data found." });
+  }
 
-    res.status(200).json(mapData)
-})
+  res.status(200).json(mapData.rows);
+});
 
-router.get('/frontPage', async (req: Request, res: Response) => {
-    const mapData = await Map.find().sort("mapName").select("-coordinates").lean().exec()
+router.get("/frontPage", async (req: Request, res: Response) => {
+  //   const mapData = await Map.find()
+  //     .sort("mapName")
+  //     .select("-coordinates")
+  //     .lean()
+  //     .exec();
 
+  const mapData = await pool.query("SELECT id, mapname, imgkey FROM maps");
 
-    if (!mapData) {
-        return res.status(400).json({ message: "No map data found."})
-    }
+  if (!mapData) {
+    return res.status(400).json({ message: "No map data found." });
+  }
 
-    res.status(200).json(mapData)
-})
+  res.status(200).json(mapData.rows);
+});
 
-router.get('/:mapID', async (req: Request, res: Response) => {
-    const mapID = req?.params?.mapID
+router.get("/:mapID", async (req: Request, res: Response) => {
+  const mapID = req?.params?.mapID;
 
-    const idCheck = mongoose.isValidObjectId(mapID)
+  //   const idCheck = mongoose.isValidObjectId(mapID);
 
-    if (!idCheck) {
-        return res.status(404).json({ message: "Invalid map ID."})
-    }
+  //   if (!idCheck) {
+  //     return res.status(404).json({ message: "Invalid map ID." });
+  //   }
 
-    const mapData = await Map.findById(mapID).lean().exec()
+  if (!mapID) {
+    return res.status(400).json({ message: "Must provide map ID" });
+  }
 
-    if (!mapID) {
-        return res.status(404).json({ message: "Map not found."})
-    }
+  //   const mapData = await Map.findById(mapID).lean().exec();
+  const mapData = await pool.query("SELECT * FROM maps WHERE id = $1", [mapID]);
 
-    res.status(200).json(mapData)
-})
+  if (!mapID) {
+    return res.status(404).json({ message: "Map not found." });
+  }
 
-export default router
+  res.status(200).json(mapData.rows[0]);
+});
+
+export default router;
