06e244bac3986218750571e7b2f1302c263d14d5
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-10 14:51:07 +0900

    Update types

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,9 +1,10 @@
 const Pool = require("pg").Pool;
+import { Pool as PoolType } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
+const pool: PoolType = new Pool({
   user: "postgres",
   password: process.env.PG_PASS,
   host: "localhost",
