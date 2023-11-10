96e665cc5aac1a3e9139c5d16a244f66b0a2cec0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-10 15:20:42 +0900

    Remove code related to MongoDb

--- src/server.ts
+++ src/server.ts
@@ -5,23 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3500;
-// const dbURI: string = process.env.DATABASE_URI!
-
-// const connectDB = async () => {
-//     try {
-//       await mongoose.connect(dbURI);
-//     } catch (err) {
-//       console.log(err);
-//     }
-// };
-
-// connectDB()
 
 const app = createServer();
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-// mongoose.connection.once('open', () => {
-//     console.log('Connected to DB')
-//     app.listen(PORT, () => console.log(`Server running on ${PORT}`))
-// })
