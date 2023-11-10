adfc17651873b1b54ec752eef14f98ab5c8f979d
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-10 15:20:09 +0900

    Remove MongoDB code

--- src/routes/mapRoutes.ts
+++ src/routes/mapRoutes.ts
@@ -1,12 +1,9 @@
 import { Router, Request, Response } from "express";
-import mongoose from "mongoose";
-import Map from "../models/Map";
 import pool from "../config/dbConfig";
 
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  // const mapData = await Map.find().sort("mapName").lean().exec()
   const mapData = await pool.query("SELECT * FROM maps");
 
   if (!mapData) {
@@ -17,12 +14,6 @@ router.get("/", async (req: Request, res: Response) => {
 });
 
 router.get("/frontPage", async (req: Request, res: Response) => {
-  //   const mapData = await Map.find()
-  //     .sort("mapName")
-  //     .select("-coordinates")
-  //     .lean()
-  //     .exec();
-
   const mapData = await pool.query("SELECT map_id, mapname, imgkey FROM maps");
 
   if (!mapData) {
@@ -35,17 +26,10 @@ router.get("/frontPage", async (req: Request, res: Response) => {
 router.get("/:mapID", async (req: Request, res: Response) => {
   const mapID = req?.params?.mapID;
 
-  //   const idCheck = mongoose.isValidObjectId(mapID);
-
-  //   if (!idCheck) {
-  //     return res.status(404).json({ message: "Invalid map ID." });
-  //   }
-
   if (!mapID) {
     return res.status(400).json({ message: "Must provide map ID" });
   }
 
-  //   const mapData = await Map.findById(mapID).lean().exec();
   const mapData = await pool.query("SELECT * FROM maps WHERE map_id = $1", [
     mapID,
   ]);
