d046223397dff7c935432b8033676a117a75fa18
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-10 15:18:23 +0900

    Fix bug with timeStamp in post route

--- src/routes/leaderboardRoutes.ts
+++ src/routes/leaderboardRoutes.ts
@@ -37,7 +37,7 @@ router.get("/", async (req: Request, res: Response) => {
   // }
 
   const mapData: QueryResult = await pool.query(
-    "SELECT maps.mapname, maps.map_id, array_agg(CASE WHEN l.playername IS NULL THEN NULL ELSE jsonb_build_object('playername', l.playername, 'timer', l.timer, 'seconds', l.seconds) END) as leaderData FROM maps left join lateral (SELECT map_id, playername, timer, seconds FROM leaderboard WHERE maps.map_id = map_id ORDER BY seconds, created_at LIMIT 5) as l ON l.map_id = maps.map_id GROUP BY maps.mapname, maps.map_id"
+    "SELECT maps.mapname, maps.map_id, array_agg(CASE WHEN l.playername IS NULL THEN NULL ELSE jsonb_build_object('leader_id', l.leader_id, 'playername', l.playername, 'timer', l.timer, 'seconds', l.seconds) END) as leaderData FROM maps left join lateral (SELECT map_id, leader_id, playername, timer, seconds FROM leaderboard WHERE maps.map_id = map_id ORDER BY seconds, created_at LIMIT 5) as l ON l.map_id = maps.map_id GROUP BY maps.mapname, maps.map_id"
   );
 
   res.status(200).json(mapData.rows);
@@ -61,7 +61,7 @@ router.post("/", async (req: Request, res: Response) => {
   const timeStamp = date.toUTCString();
 
   await pool.query(
-    "INSERT INTO leaderboard (playername, seconds, timer, map_id, created_at) VALUES ($1, $2, $3, $4)",
+    "INSERT INTO leaderboard (playername, seconds, timer, map_id, created_at) VALUES ($1, $2, $3, $4, $5)",
     [playerName, seconds, timer, mapID, timeStamp]
   );
 
